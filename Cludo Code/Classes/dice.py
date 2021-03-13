d754e60cf80c612c48e5026a59c94e7d77767330
Alex <al3xp3arc3@gmail.com>
2021-03-13 21:06:36 +0000

    put dice into class

--- Cludo Code/Classes/dice.py
+++ Cludo Code/Classes/dice.py
@@ -1,14 +1,16 @@
 import pygame
 import random
 import numpy as np
+
+
 class dice:
     def __init__(self):
-        self.rolldice(self)
+        self.rolldice()
 
-
-    def rolldice(self):
+    @staticmethod
+    def rollice():
         dis = pygame.Overlay.display.set_mode((300, 300))
-       # dice functionality
+        # dice functionality
         dis = pygame.Overlay.display.set_mode((300, 300))
         pygame.Overlay.display.set_caption("dice")
         end = False
@@ -16,35 +18,35 @@ class dice:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     end = True
-                if event.type== pygame.KEYDOWN:
-                    if event.key==pygame.K_d:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_d:
                         d = random.randint(1, 6)
-                    if d==1:
-                        pygame.draw.rect(dis,(255, 0, 0), [120, 120, 70, 70])
-                        pygame.draw.rect(dis, (0,0,0), [150, 150, 15, 15])
-                    elif d==2:
+                    if d == 1:
+                        pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
+                        pygame.draw.rect(dis, (0, 0, 0), [150, 150, 15, 15])
+                    elif d == 2:
                         pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 170, 15, 15])
-                    elif d==3:
+                    elif d == 3:
                         pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
                         pygame.draw.rect(dis, (0, 0, 0), [150, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 170, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 170, 15, 15])
-                    elif d==4:
+                    elif d == 4:
                         pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 170, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 170, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 130, 15, 15])
-                    elif d==5:
+                    elif d == 5:
                         pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 170, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 170, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [150, 150, 15, 15])
-                    elif d==6:
+                    elif d == 6:
                         pygame.draw.rect(dis, (255, 0, 0), [120, 120, 70, 70])
                         pygame.draw.rect(dis, (0, 0, 0), [130, 130, 15, 15])
                         pygame.draw.rect(dis, (0, 0, 0), [170, 170, 15, 15])
@@ -54,5 +56,6 @@ class dice:
                         pygame.draw.rect(dis, (0, 0, 0), [170, 150, 15, 15])
 
         pygame.Overlay.display.update()
+
     pygame.quit()
     quit()
