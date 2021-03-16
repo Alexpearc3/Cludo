4fa6bf9ab3c90804e4fb9daa63ba1816e6925fbf
Abby Le <tl366@sussex.ac.uk>
2021-03-16 17:32:48 +0000

    Added text

--- Cludo Code/Classes/notepad.py
+++ Cludo Code/Classes/notepad.py
@@ -4,6 +4,10 @@ import pygame
 def notepad():
     # initializing the constructor
     pygame.init()
+    # size of window
+    x = 400
+    y = 900
+    screen = pygame.display.set_mode((x, y))
     # title of window
     pygame.display.set_caption('Notepad')
     # icon of window
@@ -13,21 +17,53 @@ def notepad():
     icon = pygame.image.load(icon_img)
     pygame.display.set_icon(icon)
 
-    # size of window
-    screen = pygame.display.set_mode((400, 900))
-    done = False
-    while not done:
-        for event in pygame.event.get():
-            # quit window of room
-            if event.type == pygame.QUIT:
-                done = True
-            pygame.display.flip()
+    # define the RGB value
+    gray = (130, 130, 130)
 
-    # background
+    # create a font object (font file,size)
+    card_category_font = pygame.font.SysFont('timesnewroman', 25, bold=1)
+    card_name_font = pygame.font.SysFont('timesnewroman', 20)
+
+    # write text and assign color
+    suspects = card_category_font.render('Suspects', True, gray)
+    weapons = card_category_font.render('Weapons', True, gray)
+    rooms = card_category_font.render('Rooms', True, gray)
+
+    mustard = card_name_font.render('Mustard', True, gray)
+    green = card_name_font.render('Green', True, gray)
+    peacock = card_name_font.render('Peacock', True, gray)
+    scarlett = card_name_font.render('Scarlett', True, gray)
+    rose = card_name_font.render('Rose', True, gray)
+    azure = card_name_font.render('Azure', True, gray)
+
+    # infinite loop
+    while True:
+        # background
         notepad_background_img = "../Image/notepad_background.png"
         notepad_background = pygame.image.load(notepad_background_img)
-        screen.fill((0, 0, 0))
         screen.blit(notepad_background, (0, 0))
+
+        # set up text position and copy the text surface object to the display surface object
+        screen.blit(suspects, (58, 115))  # +200
+        screen.blit(weapons, (56, 315))
+        screen.blit(rooms, (56, 515))
+
+        screen.blit(mustard, (70, 149))  # +29
+        screen.blit(green, (70, 178))
+        screen.blit(peacock, (70, 207))
+        screen.blit(scarlett, (70, 236))
+        screen.blit(rose, (70, 265))
+        screen.blit(azure, (70, 294))
+
+        # iterate over the list of Event objects that was returned by pygame.event.get() method.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                # quit the program.
+                quit()
+
+            # Draws the surface object to the screen.
+            pygame.display.update()
 
 
 def innit():
