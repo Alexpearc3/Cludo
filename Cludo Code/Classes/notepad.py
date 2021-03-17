a8b5010fb7303cc10224eba93ae4a94796d058cd
Abby Le <tl366@sussex.ac.uk>
2021-03-17 00:26:58 +0000

    Take note by drawing on notepad

--- Cludo Code/Classes/notepad.py
+++ Cludo Code/Classes/notepad.py
@@ -4,9 +4,11 @@ import pygame
 def notepad():
     # initializing the constructor
     pygame.init()
+    import pdb
+
     # size of window
     x = 400
-    y = 900
+    y = 800
     screen = pygame.display.set_mode((x, y))
     # title of window
     pygame.display.set_caption('Notepad')
@@ -19,6 +21,7 @@ def notepad():
 
     # define the RGB value
     gray = (130, 130, 130)
+    red = (255, 0, 0)
 
     # create a font object (font file,size)
     card_category_font = pygame.font.SysFont('timesnewroman', 25, bold=1)
@@ -55,15 +58,41 @@ def notepad():
         screen.blit(rose, (70, 265))
         screen.blit(azure, (70, 294))
 
-        # iterate over the list of Event objects that was returned by pygame.event.get() method.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                # quit the program.
-                quit()
+        ################## event
+        clock = pygame.time.Clock()
+        loop = True
+        press = False
+        while loop:
+            try:
+                # pygame.mouse.set_visible(False)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        loop = False
 
-            # Draws the surface object to the screen.
-            pygame.display.update()
+                px, py = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed() == (1, 0, 0):
+                    pygame.draw.rect(screen, red, (px, py, 3, 3))
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    press = False
+                pygame.display.update()
+                clock.tick(100000)
+            except Exception as e:
+                print(e)
+                pygame.quit()
+        pygame.quit()
+        pdb.set_trace()
+        #################################################################
+
+        # iterate over the list of Event objects that was returned by pygame.event.get() method.
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         pygame.quit()
+        #         # quit the program.
+        #         quit()
+        #
+        #     # Draws the surface object to the screen.
+        #     pygame.display.update()
 
 
 def innit():
