9b877aea8118db235513c5fe1ba02d9e3e5fb0a7
Alex <al3xp3arc3@gmail.com>
2021-03-17 21:57:03 +0000

    Added player class and methods

--- Cludo Code/Classes/Player.py
+++ Cludo Code/Classes/Player.py
@@ -3,6 +3,10 @@ import Deck as deck
 
 class Player:
     cards = []
+    pId = "player ID"
+    name = "player name"
+    position = "doggy"
+    hasAccused = False
 
     def __init__(self):
         do = "what a player do"
