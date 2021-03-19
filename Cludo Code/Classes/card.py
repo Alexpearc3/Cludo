cf051332bfe5bb715797f6671ff3f367a3e22e7a
Alex <al3xp3arc3@gmail.com>
2021-03-19 00:37:01 +0000

    updating classes to be proper

--- Cludo Code/Classes/card.py
+++ Cludo Code/Classes/card.py
@@ -2,9 +2,6 @@ import pygame
 
 
 class Card:
-    # what attributes do i need?
-
     def __init__(self, image_name, card_name):
         self.image_name = image_name
         self.card_name = card_name
-        # image = pygame.image.load(self.image_name)
