684928eb46a4f1ce88a445443154819f5d5eb847
Abby Le <tl366@sussex.ac.uk>
2021-04-18 12:41:32 +0100

    updating classes to be proper

--- Cludo Code/Classes/notepad.py
+++ Cludo Code/Classes/notepad.py
@@ -1,11 +1,10 @@
 import pygame
-from pygame import mouse, event
-
+import pyttsx3
+import speech_recognition
 
 def notepad():
     # initializing the constructor
     pygame.init()
-    import pdb
 
     # size of window
     x = 400
@@ -38,8 +37,8 @@ def notepad():
 
     # initialise an object of room_cards and use the method getNames to get room_list
     room_list = room_cards.Room_cards().getNames()  # name of file and name of class
-    suspect_list = suspect_cards.Suspect_cards().getNames()     # name of file and name of class
-    weapon_list = weapon_cards.Weapon_cards().getNames()     # name of file and name of class
+    suspect_list = suspect_cards.Suspect_cards().getNames()  # name of file and name of class
+    weapon_list = weapon_cards.Weapon_cards().getNames()  # name of file and name of class
 
     # font
     card_category_font = pygame.font.SysFont('timesnewroman', 24, bold=1)
@@ -49,7 +48,7 @@ def notepad():
     general_list = []
     general_list += suspect_list + weapon_list + room_list
     general_list.insert(0, 'Suspect')
-    general_list.insert(len(suspect_list)+1, 'Weapon')
+    general_list.insert(len(suspect_list) + 1, 'Weapon')
     general_list.insert(-len(room_list), 'Room')
 
     # write items on notepad
@@ -58,36 +57,75 @@ def notepad():
         if (item == 'Suspect') or (item == 'Weapon') or (item == 'Room'):
             screen.blit(card_category_font.render(item, True, gray), (70, spacing))
         else:
-            screen.blit(card_name_font.render(item, True, gray), (85, spacing))
+            u=screen.blit(card_name_font.render(item, True, gray), (85, spacing))
         spacing += 29
+
+    # text to speech
+    while True:
+        pygame.display.update()
+        robot_ear = speech_recognition.Recognizer()
+        robot_mouth = pyttsx3.init()
+        robot_brain = ''
+
+        robot_mouth.say("You can click on the card names to take note")
+        if pygame.init():
+            robot_mouth.runAndWait()
+            break
+
+
+        with speech_recognition.Microphone() as mic:
+            audio = robot_ear.listen(mic)
+        try:
+            you = robot_ear.recognize_google(audio)
+        except:
+            you == ""
+        print(you)
+
+        if you == "Hello":
+            robot_brain == "Hi"
+        elif you == "What's your name?":
+            robot_brain == "My name is Cluedo"
+        else:
+            robot_brain == "I don't understand what you say"
+        print(robot_brain)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
 
     # quit button
     while True:
         spacing_2 = 153
-
         for even in pygame.event.get():
-
             if even.type == pygame.QUIT:
                 pygame.quit()
+
+            # button
+            a = 145
+            b = 166
+            pos = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
             if even.type == pygame.MOUSEBUTTONDOWN:
-                for element in general_list:
-                    if 85 < mouse[1] < spacing_2:
+                for i in u:
+                    # if pos == i.__pos__():
+                    if x/5.3 < pos[1] < x/2 and a < pos[1] < b:
                         pygame.draw.rect(screen, red, (85, spacing_2, 145, 2))
-                        spacing_2 += 29
-    # click button - red line
-    #     spacing_2 = 153
-    # #         pygame.draw.rect(screen, red, (x/5, y/5.9, 145, 2))
-    #     for element in general_list:
-    #         pygame.draw.rect(screen, red, (85, spacing_2, 145, 2))
-    #         spacing_2 += 29
-            # if ev.type == pygame.MOUSEBUTTONDOWN:
-            #     spacing = 159
-            #     for i in card_name_font:
-            #         if spacing - 10 <= mouse[1] <= spacing + 10:
-            #             #
-            #             pygame.draw.rect(screen, red, [70, spacing, 70, 2])
-            # #
-            #         pygame.draw.rect(screen, red, [70, 162, 70, 2])
+                # spacing_2 += 29
+                # break
+        pygame.display.update()
+
+        # click button - red line
+        #     spacing_2 = 153
+        # #         pygame.draw.rect(screen, red, (x/5, y/5.9, 145, 2))
+        #     for element in general_list:
+        #         pygame.draw.rect(screen, red, (85, spacing_2, 145, 2))
+        #         spacing_2 += 29
+        # if ev.type == pygame.MOUSEBUTTONDOWN:
+        #     spacing = 159
+        #     for i in card_name_font:
+        #         if spacing - 10 <= mouse[1] <= spacing + 10:
+        #             #
+        #             pygame.draw.rect(screen, red, [70, spacing, 70, 2])
+        # #
+        #         pygame.draw.rect(screen, red, [70, 162, 70, 2])
         #         if 110 <= mouse[0] <= 210:
         #             pygame.draw.rect(screen, red, [70, 191, 70, 2])
         #         if 58 <= mouse[2] <= 125:
@@ -101,17 +139,16 @@ def notepad():
         # else:
         #     pygame.draw.rect(screen,red,[x/2,y/2,140,40])
 
-        pygame.display.update()
 
 # # iterate over the list of Event objects that was returned by pygame.event.get() method.
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         pygame.quit()
-    #         # quit the program.
-    #         quit()
-    #
-    #     # Draws the surface object to the screen.
-    #     pygame.display.update()
+# for event in pygame.event.get():
+#     if event.type == pygame.QUIT:
+#         pygame.quit()
+#         # quit the program.
+#         quit()
+#
+#     # Draws the surface object to the screen.
+#     pygame.display.update()
 
 
 def innit():
