98a0c2eec70b750bc34c93ddf9ef411f5d5c330a
Alex <al3xp3arc3@gmail.com>
2021-04-26 16:09:17 +0100

    update board.py

--- Cludo Code/Classes/room.py
+++ Cludo Code/Classes/room.py
@@ -1,2 +1,26 @@
+
 class room:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.doors = []
+        self.players = []
+
+    def getDoors(self):
+        return self.doors
+
+    def getName(self):
+        return self.name
+
+    def setDoors(self, doorLocationX, doorLocationY):
+        self.doors.append([doorLocationX, doorLocationY])
+
+    def getPlayers(self):
+        return self.players
+
+    def setPlayers(self, player):
+        self.players.append(player)
+
+
+
+
+
