a6929c6594dd4e919954f5543dd70608c519cac3
Alex <al3xp3arc3@gmail.com>
2021-04-27 01:03:23 +0100

    updated card.py

--- Cludo Code/Classes/card.py
+++ Cludo Code/Classes/card.py
@@ -2,3 +2,9 @@ class Card:
     def __init__(self, image_name, card_name):
         self.image_name = image_name
         self.card_name = card_name
+
+    def getName(self):
+        return self.card_name
+
+    def getImgName(self):
+        return self.image_name
