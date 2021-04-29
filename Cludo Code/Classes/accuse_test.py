340de9b6639424af9cbe8d1c9341d0c000737ec1
mj349 <72075352+mj349@users.noreply.github.com>
2021-04-29 21:39:46 +0100

    Added icon and background

--- Cludo Code/Classes/accuse_test.py
+++ Cludo Code/Classes/accuse_test.py
@@ -1,13 +1,17 @@
 import accuse
 import Deck
 import Player
+import board
 
 d = Deck.Deck()
 d.init()
+plys = ["pl1","pl2","pl3","pl4","pl5","pl6"]
+bo = board.board(plys, 1)
 d.initEnvelope()
 d.shuffle()
-
-player1 = Player.Player("abby", 1)
+currentPlayer = bo.getCurrentPlayer()
+notepad = currentPlayer.getNotepad()
+player1 = Player.Player("abby", 1,  notepad)
 a = accuse.Accuse
 b = a(player1, d.getEnvelope())
 b.displayScreen()
